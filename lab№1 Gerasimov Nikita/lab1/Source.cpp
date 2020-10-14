81eade6f07c891662b956c3a8b0f019c524ec983
nikitoses <gerasimov.aa1905@asugubkin.ru>
2020-10-14 04:05:50 +0300

    Считывание И создание текстового файла

--- lab№1 Gerasimov Nikita/lab1/Source.cpp
+++ lab№1 Gerasimov Nikita/lab1/Source.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <fstream>
 
 using namespace std;
-
 
 bool run=true;
 
@@ -24,6 +24,10 @@ struct Pipe {
 
 };
 
+void change() {
+    cout << "Введите изменения: ";
+}
+
 void Menu(){
     cout << "1.Добавить трубу\
 		   \n2.Добавить КС\
@@ -33,30 +37,111 @@ void Menu(){
            \n6.Сохранить\
            \n7.Загрузить\
            \n0.Выход";
-    cout << "\n\nВвод: ";
+    cout << "\n\nВвод:\t";
 }
 
-void AddPipe(){
-  
-  /*  cout << "Введите длинну трубы: "<<Truba.lenght << endl;
-    cout << "Введите диаметр трубы: " << Truba.diameter << endl;
-    cout << "Введите ID" << Truba.id << endl;
-    cout << "Введите Дееспособность трубы" << Truba.WorkingOrNotWorking << endl;
-    return 0;*/
+int AddPipe(){
+    setlocale(LC_ALL, "ru");
+    int r;
+    ofstream EditPipe;
+    EditPipe.open("Даныне по Трубам.txt");
+    if (!EditPipe) {
+        cout << "Ошибка открытия файла";
+    }
+    else {
+        cout << "Сколько новых труб вы хотите внести в данные?" << endl;
+        cout << "Введите число:\t" << endl;
+        cin >> r;
+        for (int i = 1; i <= r; i++) {
+            string a;
+            cout << "Введите id трубы " << i<<":\t";
+            cin >> a;
+            EditPipe << a << endl;
+            
+        }
+        cout << "\n" << endl;
+               
+    }
+    
+    
+    
+    return 0;
 }
 
 
-
-void AddCompressionStation() {
-
-   
+int AddCompressionStation() {
+    setlocale(LC_ALL, "ru");
+    ofstream EditCS;
+    string b;
+    EditCS.open("Данные по КС.txt");
+    if (!EditCS) {
+        cout << "Файл не создался.";
+    }
+    else {
+       for (int i = 1; i < 2; i++) {
+           cout << "Ввод" << endl;
+           cin >> b;
+           EditCS << b << endl;
+        }
+    }
+    EditCS.close();
+    return 0;
 }
 
-//void EditPipe() {
-   
+void ViewAllObject() {
+    ifstream fin;
+    string path = ("Даныне по Трубам.txt");
+    fin.open(path);
+    if (!fin.is_open()){
+        cout << "Ошибка открытия файла" << endl;
+    }
+    else {
+        cout << "Файл открыт" << endl;
+        char ch;
+        while (fin.get(ch)) {
+            cout << ch << "\n" << endl;
+        }
+    }
+}
+
+
+int ChangePipe() {
+    setlocale(LC_ALL, "ru");
+    int n;
+    Pipe truba;
+    cout << "1.Введите новое ID трубы" << endl;
+    cout << "2.Изменить длину трубу" << endl;
+    cout << "3.Изменить диаметр трубы" << endl;
+    cout << "4.Признак" << endl;
+    cout << "Ввод:\t";
+    cin >> n;
+    switch (n)
+    {
+    case 1:
+        change();
+        cin >> truba.id;
+        break;
+    case 2:
+        change();
+        cin >> truba.lengh;
+        break;
+    case 3:
+        change();
+        cin >> truba.diameter;
+        break;
+    case 4:
+        change();
+        cin >> truba.WorkingOrNotWorking;
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
 
 int EditCompressionStation() {
-    setlocale(LC_ALL, "rus");
+    setlocale(LC_ALL, "ru");
     CompressionStation station;
     int n;
 
@@ -72,7 +157,10 @@ int EditCompressionStation() {
         cout << station.NumberOfWorkShops;
         break;
     case 2:
+        
         cout << "Введите новое название КС" << endl;
+        cin >> station.name;
+        cout << station.name << endl;
         break;
     default:
         break;
@@ -82,14 +170,12 @@ int EditCompressionStation() {
 
 
 int main() {
-    setlocale(LC_ALL, "rus");
+    setlocale(LC_ALL, "ru");
 
     int n;
     string a;
 
-   
-    
-    Menu();
+     Menu();
     
     while (run) {
         cin >> n;
@@ -98,21 +184,31 @@ int main() {
         switch (n)
         {
         case 1:
-              
+            AddPipe();
+            run = true;
+            Menu();
             break;
         case 2:
-         
+            AddCompressionStation();
             break;
         case 3:
-            
-
+            ViewAllObject();
+            run = true;
+            Menu();
             break;
         case 4:
-           
+            ChangePipe();
+            run = true;
+            Menu();
             break;
         case 5:
 
             EditCompressionStation();
+            run = true;
+            cout << " " << endl;
+            Menu();
+            
+
             break;
 
         case 6:
@@ -122,11 +218,14 @@ int main() {
                
             break;
         case 0:
-            
+            run = false;
             
             break;
 
         default:
+            cout << "Выберите Из придложенного в меню\n" << endl;
+            
+            
             run = true;
             Menu();
 
